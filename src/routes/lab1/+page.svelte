501cd39a587b5285bf06b261a6cca2a7096cd92b
Prycom <as.gorohov2003@gmail.com>
2024-12-19 03:27:14 +0700

    troubles with ts; trying to reinstall project

--- src/routes/lab1/+page.svelte
+++ src/routes/lab1/+page.svelte
@@ -22,27 +22,39 @@
 	let maxChars = 5;
 
 	function generateChains(rules: Rule[], minChars: number, maxChars: number) {
-		let chains = ['S'];
+		let chains: string[] = ['S'];
 		let output: string[] = [];
 
 		while (chains.length > 0) {
-			const chain = chains.shift() as string;
-			const newChains: string[] = [];
-
-			for (const rule of rules) {
-				for (const key in rule) {
-					if (chain.includes(key)) {
-						console.log(`key: ${key}`)
-						
-						const replacement = rule[key][Math.floor(Math.random() * rule[key].length)];
-						const newChain = chain.replace(key, replacement);
-						newChains.push(newChain);
+			let chain = chains.shift() as string; // get chain and delete it from chains
+			let newChains: string[] = [];
+			console.log(chains)
+			if (chain.length > maxChars){
+				console.log(`max chars for "${chain}"`)
+				continue
+			}
+			for (const rule of rules) { // get every rule object
+				for (const key in rule) { // get key of rule 
+					if (chain.includes(key)) { // if terminal in chain
+						//console.log(`key: ${key}; chain: ${chain}`)
+						// for every variant for particular key
+						for(const variant of rule[key]){
+							//console.log(`${variant}`)
+							const newChain = chain.replace(key, variant)
+							//console.log(`chain: ${chain}; new chain: ${newChain}`)
+							newChains.push(newChain)
+							//if (newChain.length >= minChars && newChain <= maxChars && !newChain.includes())
+						}
 					}
 				}
 			}
-
+			if (chains.length > 100){
+				break
+			}
+			//console.log(newChains)
 			if (newChains.length > 0) {
 				chains.push(...newChains);
+				newChains = []
 			} else {
 				if (chain.length >= minChars && chain.length <= maxChars) {
 					output.push(chain);
@@ -63,7 +75,7 @@
 	<p class="text-2xl italic">Пример</p>
 
 	<div class="flex flex-col gap-8 lg:flex-row">
-		<div class="card w-96 bg-base-200 shadow-xl">
+		<div class="card lg:w-96 bg-base-200 shadow-xl">
 			<div class="card-body">
 				<div class="card-title mb-2">Правила:</div>
 				{#each exRules as rule}
@@ -85,7 +97,7 @@
 					{/each}
 				</div>
 				<div class="card-actions mt-4 justify-center">
-					<button class="btn btn-primary" on:click={generateChains(exRules, 3, 5)}>Сгенерировать!</button>
+					<button class="btn btn-primary" on:click={generateChains(exRules, 2, 3)}>Сгенерировать!</button>
 				</div>
 			</div>
 		</div>
